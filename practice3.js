3a0312338047aed66b6015e1c707a862af8221b8
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2023-07-19 12:42:08 +0330

    square and recktangle solution fixed

--- practice3.js
+++ practice3.js
@@ -1,13 +1,13 @@
 // solution for square and recktangle
 
-let height = prompt('Enter the height');
-let width = prompt('Enter the width');
+let height = parseInt(prompt('Enter the height'));
+let width = parseInt(prompt('Enter the width'));
 
 let area = width*height;
-let peri = 2*( width + height );
+let peri = 2 * ( width + height );
 
 
-if ( width = height ) {
+if ( width == height ) {
     console.log(area);
 } else {
     console.log(peri);
