02044badc8b8b8fb69ecf97376bb61555cb0193c
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2023-07-19 12:56:17 +0330

    changes added to BMI

--- practice1.js
+++ practice1.js
@@ -1,15 +1,17 @@
 // solution for BMI
 
-let weight = prompt('Enter your weight');
-let height = prompt('Enter your height');
+let weight = prompt('Enter your weight in kg');
+let height = prompt('Enter your height in cm');
 
-let BMI = (weight / (height*height));
+
+let heightInmeter = height * 0.01; 
+let BMI = (weight / (heightInmeter*heightInmeter));
 
 if (BMI <= 18.4) {
     console.log('underweight');
 } else if (BMI <= 24.9 && BMI > 18.5) {
     console.log('Normal');
-} else if (25.0 <= BMI && BMI < 39.9) {
+} else if (  BMI < 39.9  &&  BMI >= 25.0) {
     console.log('Overweight');
 } else {
     console.log('Obese')
