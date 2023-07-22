423527c46242e9e89b0a30f650277954bbc87813
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2023-07-22 16:08:51 +0330

    summing up  numbers

---
+++ practice 06 - summing numbers/function.js
@@ -0,0 +1,45 @@
+
+
+// solution with if
+
+function sum(input) {
+    
+    let number = Number(input);
+
+    if (number === 0) {
+        return 0;
+    }
+
+    let total = number + sum(prompt("Enter a number (or 0 to finish):"));
+
+    return total;
+}
+
+
+
+
+// solution with while loop
+
+
+// function sum(input) {
+
+//     let sum = 0;
+
+//     while (true) {
+
+//         let number = Number(input);
+
+//         sum += number;
+
+//         if (number === 0) {
+//             return sum;
+//         }
+
+        
+
+//         input = prompt("Enter a number (or 0 to finish):");
+//     }
+// }
+
+
+
