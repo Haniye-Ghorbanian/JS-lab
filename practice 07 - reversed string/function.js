7bbe29689f9c619db0b805a8ab42d37780d6ea7b
Haniye Ghorbanian <haniyeghorbanian79@gmail.com>
2023-07-24 14:18:06 +0330

    Reversed String Practice (not working yet)

---
+++ practice 07 - reversed string/function.js
@@ -0,0 +1,15 @@
+
+
+
+function ReverseString(string) {
+
+    let reversed = '';
+
+    for (let i = string.length - 1; i >= 0; i--) {
+      reversed += string[i];
+    }
+    return reversed;
+  }
+  
+  
+  
---
+++ practice 07 - reversed string/main.js
@@ -0,0 +1,9 @@
+
+// Prompt the user to enter a word
+let UserString = prompt("Enter a word to reverse")
+
+// Call the reverseString function with the user input
+let reversed = ReverseString(UserString);
+
+// Display the reversed string to the user
+alert('The reversed string is ' + '"' + reversed + '"');
